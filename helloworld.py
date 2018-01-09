21eb8636a700437eb948cb8e73a61341f9784811
mynameishardtoremember <35249860+mynameishardtoremember@users.noreply.github.com>
2018-01-08 21:52:14 -0800

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+def main(*unused_args):
+  print_me('hello world!');
+
+def print_me(banner):
+  print banner
+
+if __name__ == '__main__':
+  main()
